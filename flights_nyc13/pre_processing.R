89e45b9c1ed71cf5023bd6f2742dbd11ae1a0fe5
Yaara <yaara3232@gmail.com>
2022-08-20 13:29:44 +0300

    added package dataPreparation removed sd outliers removed constant and identical columns

--- flights_nyc13/pre_processing.R
+++ flights_nyc13/pre_processing.R
@@ -14,28 +14,12 @@ airports <- airports
 planes <- planes
 weather <- weather
 
-ggplot(flights) +
-  aes(x = "", y = dep_delay) +
-  geom_boxplot(fill = "#0c4c8a") +
-  theme_minimal()
-
-boxplot.stats(flights$dep_delay)$out
 
 #merged flights + weather + planes
 flights_planes<-merge(flights, planes, by="tailnum")
 flights_full<-merge(flights_planes,weather,by=c("origin","time_hour"))
+summary(flights_full)
 flights_full$hour.y<-as.numeric(flights_full$hour.y)
-
-#remove identical columns
-identical(flights_full$hour.x, flights_full$hour.y)
-identical(flights_full$month.x, flights_full$month.y)
-identical(flights_full$day.x, flights_full$day.y)
-
-#remove columns
-flights_full <- select(flights_full, -c(time_hour, arr_delay))
-
-#display all flights_full
-str(flights_full)
 
 # Make dependent variable as a factor (categorical)
 flights_full$origin = as.factor(flights_full$origin)
@@ -47,5 +31,36 @@ flights_full$manufacturer = as.factor(flights_full$manufacturer)
 flights_full$model = as.factor(flights_full$model)
 flights_full$engine = as.factor(flights_full$engine)
 
+#identical columns
+identical(flights_full$hour.x, flights_full$hour.y)
+identical(flights_full$month.x, flights_full$month.y)
+identical(flights_full$day.x, flights_full$day.y)
+
+#remove identical and constant columns
+flights_full <- fast_filter_variables(
+  flights_full,
+  level = 2,
+  keep_cols = NULL,
+  verbose = TRUE
+)
+
 #remove NA from dep_delay
 flights_full<-flights_full%>% drop_na(dep_delay)
+
+#outliers
+ggplot(flights) +
+  aes(x = "", y = dep_delay) +
+  geom_boxplot(fill = "#0c4c8a") +
+  theme_minimal()
+
+boxplot.stats(flights$dep_delay)$out
+
+#remove sd outliers
+flights_full <- remove_sd_outlier(flights_full, cols = "dep_delay", n_sigmas = 7, verbose = TRUE)
+
+#display all flights_full
+str(flights_full)
+
+
+
+
