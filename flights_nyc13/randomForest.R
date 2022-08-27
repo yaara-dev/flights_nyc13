b0cff232f7d8adeec8de1860ed8127fb8d35d433
Tal Goldberg <talg97@gmail.com>
2022-08-27 17:36:20 +0300

    modified:   flights_nyc13/randomForest.R

--- flights_nyc13/randomForest.R
+++ flights_nyc13/randomForest.R
@@ -1,8 +1,16 @@
-outlier_detect <- function(variable, n){
- std <- sd(variable)
- for (val in variable) {
-  if(val> n*std){
-    val <- NA
-  } 
- }return(variable)
-}
+library(randomForest)
+require(caTools)
+set.seed(101)
+sample = sample.split(flights_full$dep_delay, SplitRatio = .75)
+train = subset(flights_full, sample == TRUE)
+test  = subset(flights_full, sample == FALSE)
+dim(train)
+dim(test)
+rf <- randomForest(
+  dep_delay ~.,
+  data=train,
+  na.action=na.roughfix
+)
+rf
+unique(flights_full$dep_delay)
+str(flights_full) 
