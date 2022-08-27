c9d280f15bdfde57f857077f978f75b8a578af81
Yaara <yaara3232@gmail.com>
2022-08-27 12:20:32 +0300

    plot numeric variables vs normalized dep_delay

--- flights_nyc13/pre_processing.R
+++ flights_nyc13/pre_processing.R
@@ -37,6 +37,84 @@ summary(flights_full)
 length(which(is.na(flights_full$speed)))
 length(which(is.na(flights_full$wind_gust)))
 
+##weather features
+#find variables that are similar
+numeric_weather <- sapply(weather, is.numeric)
+numeric_weather_data <- weather[numeric_weather]
+numeric_weather_data <- select(numeric_weather_data, -c(year))
+corr <-
+  round(cor(numeric_weather_data, method = "spearman", use = "complete.obs"),
+        2)
+corr
+heatmap(
+  corr,
+  Rowv = NA,
+  Colv = NA,
+  symm = TRUE,
+  margins = c(6, 6),
+)
+
+#dewp and temp are highly correlated (0.9), and so are wind speed and wind gust (0.87)
+#we already removed wind_gust because it was mostly NAs.
+#will remove dewp as well to avoid double variables in the model
+
+#### todo- fix function####
+#plot dewp vs temp
+plot(flights_full$dewp, flights_full$temp)
+get_var_df <- function(var_name) {
+  var_df <-
+    flights_full %>% group_by(get(var_name)) %>% summarise(total_counts = n(),
+                                                           mean_delay = mean(dep_delay))
+  return(var_df)
+}
+plot_var_vs_norm_dep_delay <- function(var_df) {
+  #jpeg(sprintf('%s vs normalized dep_delay.jpg', var))
+  
+  var_df$normalized_dep <-
+    var_df$mean_delay / var_df$total_counts
+  
+  ggplot(var_df, aes(x = var_df[[1]], y = normalized_dep)) + geom_bar(stat =
+                                                                        "identity") + labs(x = as.name(var))
+  
+  #dev.off()#normalized mean dep_delay per type
+}
+for (var in colnames(flights_full)) {
+  if (is.numeric(flights_full[, eval(as.name(var))])) {
+    print(as.name(var))
+    var_df <- get_var_df(c(var))
+    plot_var_vs_norm_dep_delay(var_df)
+    
+  }
+}
+
+numeric_data <- sapply(flights_full_old, is.numeric)
+numeric_data <- flights_full_old[numeric_data]
+sapply(as.vector(colnames(numeric_data)), get_var_df)
+p_df1 <- get_var_df(c('pressure'))
+plot_var_vs_norm_dep_delay(var_df)
+#check dewp variable
+dewp_df <-
+  flights_full %>% group_by(dewp) %>% summarise(total_counts = n(), mean_delay =
+                                                  mean(dep_delay))
+dewp_df$normalized_dep <-
+  dewp_df$mean_delay / dewp_df$total_counts
+ggplot(dewp_df, aes(x = dewp, y = normalized_dep)) + geom_bar(stat =
+                                                                "identity") #normalized mean dep_delay per type
+
+hist(flights_full$dewp)
+
+#check pressure variable
+pressure_df <-
+  flights_full %>% group_by(pressure) %>% summarise(total_counts = n(), mean_delay =
+                                                      mean(dep_delay))
+pressure_df$normalized_dep <-
+  pressure_df$mean_delay / pressure_df$total_counts
+ggplot(pressure_df, aes(x = pressure, y = normalized_dep)) + geom_bar(stat =
+                                                                        "identity") #normalized mean dep_delay per type
+
+hist(flights_full$pressure)
+
+######
 #remove irrelevant columns
 flights_full <-
   select(
@@ -55,7 +133,8 @@ flights_full <-
       tz,
       dst,
       speed,
-      wind_gust
+      wind_gust,
+      dewp
     )
   )
 ## cyclic features
@@ -64,31 +143,30 @@ flights_full <-
 #flights_full$month_sin = sin((flights_full$month.x - 1) * (2 * pi / 12))
 #flights_full$month_cos = cos((flights_full$month.x - 1) * (2 * pi / 12))
 
+flights_full <-
+  flights_full %>% mutate(sched_dep_time = hour.x * 60 + minute)
+#todo - sched_arr_time
+
+
 #remove NA from dep_delay
 flights_full <- flights_full %>% drop_na(dep_delay)
 flights_full <- flights_full %>% drop_na(tzone)
 
-##weather features
-#pressure
-pressure_df<-flights_full%>% group_by(pressure)%>% summarise(total_counts=n(), mean_delay=mean(dep_delay))
-pressure_df$normalized_dep<-pressure_df$mean_delay/pressure_df$total_counts
-ggplot(pressure_df, aes(x = pressure, y=normalized_dep)) + geom_bar(stat="identity") #normalized mean dep_delay per type
 
-hist(flights_full$pressure)
 
 # Make dependent variable as a factor (categorical)
 flights_full <- transform(
   flights_full,
   origin = as.factor(origin),
-  month.x = as.factor(month.x),
-  day.x = as.factor(day.x),
+  #month.x = as.factor(month.x),
+  #day.x = as.factor(day.x),
   carrier = as.factor(carrier),
   tzone = as.factor(tzone),
   type = as.factor(type),
   model = as.factor(model),
   engine = as.factor(engine),
-  month.y = as.factor(month.y),
-  day.y = as.factor(day.y),
+  #month.y = as.factor(month.y),
+  #day.y = as.factor(day.y),
   hour.y = as.numeric(hour.y),
   manufacturer = as.factor(manufacturer)
 )
