26c477c33d2d7ae950f704b9e6397f8afa44c3dd
Yaara <yaara3232@gmail.com>
2022-09-01 08:50:08 +0300

    raadd sensitivity tests

--- flights_nyc13/randomForest.R
+++ flights_nyc13/randomForest.R
@@ -3,62 +3,159 @@ install.packages('ROSE')
 library(ranger)
 library(ROSE)
 
-balanced.data <-
-  ovun.sample(
-    dep_delay ~ .,
-    flights_full_arranged,
-    method = "both",
-    p = 0.5,
-    seed = 26
-  )$data
 
-table(balanced.data$dep_delay)
-#plot flights counts per 2 dep_delay categories
-ggplot(balanced.data, aes(dep_delay, fill = dep_delay)) + geom_bar(fill =
-                                                                             c('#CC6666', '#FFCCCC')) +     #'#660000', '#993333', '#CC6666'"#FFCCCC"
-  labs(title = "Flights counts per departure delay category", x = "Departure delay categories") +
-  theme(plot.title = element_text(hjust = 0.5, face = "bold"))
-balanced.data.edit <- select(balanced.data, -c(manu_model, air_time))
+get_balanced_df <- function(flights_full_arranged){
+  balanced.data <-
+    ovun.sample(
+      dep_delay ~ .,
+      flights_full_arranged,
+      method = "both",
+      p = 0.5,
+      seed = 26
+    )$data
+  return(balanced.data)
+}
 
-sample = sample.split(balanced.data.edit$dep_delay, SplitRatio = .8)
-train = subset(balanced.data.edit, sample == TRUE)
-test  = subset(balanced.data.edit, sample == FALSE)
+balanced_data_15 <- get_balanced_df(flights_full_arranged_15)
+balanced_data <- get_balanced_df(flights_full_arranged)
+balanced_data_25 <- get_balanced_df(flights_full_arranged_25)
+
+table(balanced_data_15$dep_delay)
+table(balanced_data$dep_delay)
+table(balanced_data_25$dep_delay)
+
+train_test_split <- function(balanced_data){
+  sample = sample.split(balanced_data$dep_delay, SplitRatio = .8)
+  train = subset(balanced_data, sample == TRUE)
+  test  = subset(balanced_data, sample == FALSE)
+  train_test <- list(train, test)
+  return(train_test)
+}
+
+#15
+train_15 <- train_test_split(balanced_data_15)[[1]]
+test_15 <- train_test_split(balanced_data_15)[[2]]
+dim(train_15)
+dim(test_15)
+
+#20
+train <- train_test_split(balanced_data)[[1]]
+test <- train_test_split(balanced_data)[[2]]
 dim(train)
 dim(test)
 
-fit <- ranger(
-  dep_delay ~ .,
-  data = train,
-  num.trees = 1000,
- # max.depth = 10,
-  importance = 'impurity',
-  #scale.permutation.importance = TRUE,
-  verbose = TRUE,
- # min.node.size = 10,
-  #splitrule = "extratrees"
-)
-fit
+#25
+train_25 <- train_test_split(balanced_data_25)[[1]]
+test_25 <- train_test_split(balanced_data_25)[[2]]
+dim(train_25)
+dim(test_25)
+
+run_random <- function(train){
+  fit <- ranger(
+    dep_delay ~ .,
+    data = train,
+    num.trees = 1000,
+    importance = 'impurity',
+    verbose = TRUE,
+    
+  )
+  return(fit)
+}
+
+rf <- run_random(train)
+rf
+
+##sensitivity 
+#15
+rf_15 <- run_random(train_15)
+rf_15
+
+#25
+rf_25 <- run_random(train_25)
+rf_25
 
 
-pred <- predict(fit, test, verbose = TRUE )#$predictions
+
+pred <- predict(rf, test)
 confusion <- table(test$dep_delay, pred$predictions)
 confusion
+
+
+##sensitivity 
+#15
+pred_15 <- predict(rf_15,test_15)
+confusion_15 <- table(test_15$dep_delay, pred_15$predictions)
+confusion_15
+
+#25
+pred_25 <- predict(rf_25,test_25)
+confusion_25 <- table(test_25$dep_delay, pred_25$predictions)
+confusion_25
+
+par(mfrow=c(1,3))
 # plot confusion matrix
 plot(confusion,
      xlab = 'Predicted',
      ylab = 'True',
   main= sprintf('Predicted vs Obsvered
- OOB prediction error: %s',round(fit$prediction.error, 3))) 
+ OOB prediction error: %s',round(rf$prediction.error, 3))) 
+
+plot(confusion_15,
+     xlab = 'Predicted',
+     ylab = 'True',
+     main= sprintf('Predicted vs Obsvered, sensitivy check(15)
+ OOB prediction error: %s',round(rf_15$prediction.error, 3)))
+
+plot(confusion_25,
+     xlab = 'Predicted',
+     ylab = 'True',
+     main= sprintf('Predicted vs Obsvered, sensitivy check(25)
+ OOB prediction error: %s',round(rf_15$prediction.error, 3))) 
 
 
-imps <- data.frame(
-  var = names(train)[-5],
-  imps = fit$variable.importance / max(fit$variable.importance)
-)
-imps %>%
+get_importance <- function(rf){
+  imps <- data.frame(
+    var = names(train)[-5],
+    imps = rf$variable.importance / max(rf$variable.importance)
+  )
+  return(imps)
+}
+
+imps <- get_importance(rf)
+imps_15 <- get_importance(rf_15)
+imps_25 <- get_importance(rf_25)
+
+require(gridExtra)
+
+
+p1 <- imps %>%
   ggplot(aes(imps, x = reorder(var, imps))) +
   geom_point(size = 3, colour = "#ff6767") +
   coord_flip() +
   labs(x = "Predictors", y = "Importance scores") +
+  ggtitle('Importance')+
   theme_bw(18)
+p1
+
+p2 <- imps_15 %>%
+  ggplot(aes(imps, x = reorder(var, imps))) +
+  geom_point(size = 3, colour = "#ff6767") +
+  coord_flip() +
+  labs(x = "Predictors", y = "Importance scores") +
+  ggtitle('Importance: sensitivity(15)')+
+  theme_bw(18)
+
+
+p3 <- imps_25 %>%
+  ggplot(aes(imps, x = reorder(var, imps))) +
+  geom_point(size = 3, colour = "#ff6767") +
+  coord_flip() +
+  labs(x = "Predictors", y = "Importance scores") +
+  ggtitle('Importance: sensitivity(25)')+
+  theme_bw(18)
+
+
+
+
+grid.arrange(p1, p2, p3, ncol=2)
  
